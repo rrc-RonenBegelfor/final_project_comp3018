5edff90014e144aec2bc719d96766736ca4354f0
Ronen Begelfor <rbegelfor@rrc.ca>
2025-11-11 23:56:54 -0600

    Added city tests that cover addition, updating, and deletion.

--- test/cityController.test.ts
+++ test/cityController.test.ts
@@ -6,7 +6,7 @@ jest.mock("../src/api/v1/services/cityService");
 
 import * as cityController from "../src/api/v1/controllers/cityController";
 import * as cityService from "../src/api/v1/services/cityService";
-// import { City } from "../src/api/v1/models/cityModel";
+import { City } from "../src/api/v1/models/cityModel";
 
 describe("City Controller", () => {
     let mockReq: Partial<Request>;
@@ -38,7 +38,7 @@ describe("City Controller", () => {
             await cityController.getCity(
                 mockReq as Request,
                 mockRes as Response,
-                mockNext
+                mockNext as NextFunction,
             );
 
             // Assert
@@ -63,7 +63,7 @@ describe("City Controller", () => {
             await cityController.getCity(
                 mockReq as Request,
                 mockRes as Response,
-                mockNext
+                mockNext as NextFunction,
             );
 
             // Assert
@@ -104,7 +104,7 @@ describe("City Controller", () => {
             await cityController.getCity(
                 mockReq as Request,
                 mockRes as Response,
-                mockNext
+                mockNext as NextFunction,
             );
 
             // Assert
@@ -131,10 +131,168 @@ describe("City Controller", () => {
             await cityController.getCity(
                 mockReq as Request,
                 mockRes as Response,
-                mockNext
+                mockNext as NextFunction,
             );
 
             expect(mockNext).toHaveBeenCalledWith(mockError);
+        });
+    });
+
+    describe("createCity", () => {
+        it("should handle a successful creation", async () => {
+            // Arrange
+            const mockBody: City = {
+                countryId: "test",
+                name: "test",
+                date: "test",
+                type: "test",
+                description: "test",
+                resolution: "test",
+                damage: "test",
+            };
+
+            const mockCity: City = {
+                id: "test",
+                ...mockBody,
+            }
+
+            mockReq.body = mockBody;
+
+            (cityService.createCity as jest.Mock).mockReturnValueOnce(mockCity);
+
+            // Act
+            await cityController.createCity(
+                mockReq as Request,
+                mockRes as Response,
+                mockNext
+            );
+
+            // Assert
+            expect(mockRes.status).toHaveBeenCalledWith(HTTP_STATUS.CREATED);
+        });
+
+        it("should return a bad request due to lack of parameters", async () => {
+            // Act
+            await cityController.createCity(
+                mockReq as Request,
+                mockRes as Response,
+                mockNext as NextFunction,
+            );
+
+            // Assert
+            expect(mockRes.status).toHaveBeenLastCalledWith(HTTP_STATUS.BAD_REQUEST);
+            expect(mockRes.json).toHaveBeenCalledWith({
+                details: [
+                    "\"countryId\" is required",
+                    "City name is required",
+                    "Date is required",
+                    "Event type is required",
+                    "Description is required",
+                    "Damage is required",
+                    "Resolution is required",
+                ],
+                message: "Validation failed",
+            });
+        });
+    });
+
+    describe("updateCity", () => {
+        it("should update a city successfully", async () => {
+            // Arrange
+            const mockId: string = "test";
+            const mockBody: City = {
+                countryId: "test",
+                name: "test",
+                date: "test",
+                type: "test",
+                description: "test",
+                resolution: "test",
+                damage: "test",
+            }
+
+            mockReq.params = {id: mockId};
+            mockReq.body = mockBody;
+            const mockCity: City = {
+                countryId: "test",
+                name: "test",
+                date: "test",
+                type: "test",
+                description: "test",
+                resolution: "test",
+                damage: "test",
+            };
+
+            (cityService.updateCity as jest.Mock).mockReturnValueOnce(mockCity);
+
+            await cityController.updateCity(
+                mockReq as Request,
+                mockRes as Response,
+                mockNext as NextFunction,
+            );
+
+            // Assert
+            expect(mockRes.status).toHaveBeenCalledWith(HTTP_STATUS.OK);
+            expect(mockRes.json).toHaveBeenCalledWith({
+                data: {
+                    countryId: "test",
+                    name: "test",
+                    date: "test",
+                    type: "test",
+                    description: "test",
+                    resolution: "test",
+                    damage: "test",
+                },
+                message: "City successfully updated",
+                status: "success",
+                timestamp: new Date().toISOString(),
+            });
+        });
+
+        it("should handle an error while also calling next", async () => {
+            // Arrange
+            const mockError: Error = new Error("Mock error");
+            mockReq.params = { id: "1"};
+            mockReq.body = {
+                name: "Test"
+            };
+            
+            (cityService.updateCity as jest.Mock).mockRejectedValue(mockError);
+
+            // Act
+            await cityController.updateCity(
+                mockReq as Request,
+                mockRes as Response,
+                mockNext as NextFunction,
+            );
+
+            // Assert
+            expect(mockNext).toHaveBeenCalledWith(mockError);
+        });
+    });
+
+    describe("deleteCity", () => {
+        it("should successfully delete mocked data", async () => {
+            // Arrange
+            const mockId: string = "test";
+            mockReq.params = {id: mockId};
+
+            (cityService.deleteCity as jest.Mock).mockReturnValueOnce(undefined);
+
+            // Act
+            await cityController.deleteCity(
+                mockReq as Request,
+                mockRes as Response,
+                mockNext as NextFunction,
+            );
+
+            // Assert
+            expect(mockRes.status).toHaveBeenCalledWith(HTTP_STATUS.OK);
+            expect(mockRes.json).toHaveBeenCalledWith({
+                data: "City successfully deleted",
+                message: undefined,
+                status: "success",
+                timestamp: new Date().toISOString(),
+            });
         });
     });
 });
